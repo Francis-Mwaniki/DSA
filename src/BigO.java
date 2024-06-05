ac1f5cf81defeb7b035f7ba503da1968bf9f05bb
Francis-Mwaniki <francismwaniki630@gmail.com>
2024-06-05 13:59:42 +0300

    BigO notation

--- src/BigO.java
+++ src/BigO.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BigO {
+public class BigO {
+    public int max = 1000000;
+    public static void  main(String[] args){
+    Bigi bigi = new Bigi();
+
+    int resA = bigi.addMinimalUp(2);
+    int resB = bigi.addUp(3);
+
+    System.out.println("For loop here:\t" + resB);
+    System.out.println("Minified Version:\t" + resA);
+    }
 }
--- src/Bigi.java
+++ src/Bigi.java
@@ -1,2 +1,13 @@
 public class Bigi {
+    public int addUp(int max){
+        int sum = 0;
+        for (int i = 1; i < max; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    public int addMinimalUp(int max){
+        return max * (max + 1) /2;
+    }
+
 }
