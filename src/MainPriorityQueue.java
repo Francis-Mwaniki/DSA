b998ef896d1295678a8f5f4e75c277fa32745419
Francis-Mwaniki <francismwaniki630@gmail.com>
2024-06-05 14:01:28 +0300

    Priority Queues

--- src/MainPriorityQueue.java
+++ src/MainPriorityQueue.java
@@ -1,2 +1,16 @@
+import java.util.*;
 public class MainPriorityQueue {
+    public static void main(String[] args){
+        //It's A FIFO data structure with higher priority  order
+        //can use Collections - /reverseOrder()
+        Queue<Double> queue = new PriorityQueue<>(Collections.reverseOrder());
+        queue.offer(4.0);
+        queue.offer(3.4);
+        queue.offer(0.4);
+        queue.offer(2.0);
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+    }
 }
