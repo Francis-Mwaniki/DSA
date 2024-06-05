59c1851291dbb69c108a7d0b989b617e06c5ce63
Francis-Mwaniki <francismwaniki630@gmail.com>
2024-06-05 14:00:30 +0300

    Dyamic arrays

--- src/MainDynamicArray.java
+++ src/MainDynamicArray.java
@@ -1,2 +1,23 @@
 public class MainDynamicArray {
+    public static void main(String[] args){
+        DynamicArray dynamicArray = new DynamicArray();
+
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
+        dynamicArray.add("G");
+
+        //use insert --
+        dynamicArray.insert(0,"X");
+        // delete
+        dynamicArray.delete("A");
+
+        System.out.println(dynamicArray);
+        System.out.println("Empty: " + dynamicArray.isEmpty());
+        System.out.println("Size: " + dynamicArray.size);
+        System.out.println("Capacity: " + dynamicArray.capacity);
+
+    }
 }
